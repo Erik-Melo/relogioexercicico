fd6de1b4378dd56ae24e27ed1085eda27b581fc6
Erik Melo <emelo@nwsoft.com.br>
2024-06-24 08:50:41 -0300

    Add Elemento da Data

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # relogioexercicico
 
 Relogio Digital Exercicio
+Relogio Digital com a Data - Exercicio
--- script.js
+++ script.js
@@ -3,21 +3,33 @@ const horasElement = document.getElementById("horas");
 const minutosElement = document.getElementById("minutos");
 const segundosElement = document.getElementById("segundos");
 
+const diaElement = document.getElementById("dia");
+const mesElement = document.getElementById("mes");
+const anoElement = document.getElementById("ano");
+
 // Funcao para mostrar as Horas
 function novaHora() {
   // Variaveis
-  // Data
-  const date = new Date();
+  const date = new Date(); //DATA
 
-  // Horas, Minutos e Segundos
-  const horas = date.getHours();
-  const minutos = date.getMinutes();
-  const segundos = date.getSeconds();
+  const horas = date.getHours(); //Horas
+  const minutos = date.getMinutes(); //Minutos
+  const segundos = date.getSeconds(); // Segundos
+
+  const dia = date.getDate(); //Dia
+  const mes = date.getMonth() + 1; //Mes. (Mais 1, pois é comeco do 0).
+  const ano = date.getUTCFullYear(); //Ano
+
+  // console.log(mes);
 
   // Atribuindo o valor da variavel ao elementos
   horasElement.textContent = fixtime(horas);
   minutosElement.textContent = fixtime(minutos);
   segundosElement.textContent = fixtime(segundos);
+
+  diaElement.textContent = fixtime(dia);
+  mesElement.textContent = fixtime(mes);
+  anoElement.textContent = ano;
 }
 
 // Funcao para add o zero se o valor for menor que 10
